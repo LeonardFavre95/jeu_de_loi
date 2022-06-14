33d076faa64dc50f7c43936f7e472ac9689ed262
Léonard <leo1095@gmail.com>
2022-06-14 08:01:39 +0200

    Nav_bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
+  // firebase start of the app
+//  await Firebase.initializeApp(
+//   options: DefaultFirebaseOptions.currentPlatform,
+// );
 }
 
 class MyApp extends StatelessWidget {
