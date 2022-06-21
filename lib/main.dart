e32a649e0ad275af5948cde34fb8b4134afb5d14
Léonard <leo1095@gmail.com>
2022-06-21 09:57:53 +0200

    gradle_firebase_setup

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(const MyApp());
