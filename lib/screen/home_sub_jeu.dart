09ee6f7127019345cf39884c48e76a028facaa14
LeonardFavre95 <leonard.favre@students.hevs.ch>
2022-07-22 10:27:10 +0200

    Création des Container en cascade

--- lib/screen/home_sub_jeu.dart
+++ lib/screen/home_sub_jeu.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unnecessary_const
-
 import 'package:flutter/material.dart';
 
 class HomeSubJeu extends StatelessWidget {
@@ -14,190 +12,189 @@ class HomeSubJeu extends StatelessWidget {
         reverse: true,
         crossAxisCount: 3,
         padding: const EdgeInsets.all(20),
-        children: <Widget>[
-          Container(
-            //https://stackoverflow.com/questions/45724567/flutter-boxdecoration-s-background-color-overrides-the-containers-background-co
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "Start",
-              style: TextStyle(fontSize: 20),
+        children: List.generate(18, (index) {
+          return Padding(
+            padding: const EdgeInsets.all(0),
+            child: Container(
+              //https://stackoverflow.com/questions/45724567/flutter-boxdecoration-s-background-color-overrides-the-containers-background-co
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 0.7,
+                ),
+                color: Colors.teal[100],
+              ),
+              alignment: Alignment.center,
             ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "1",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "2",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "3",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "4",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "5",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "6",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "Réponse quiz 1-6",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "Accueil",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "réponses et bonne pratique",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "Les RH et admin",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "réponses et bonne pratique",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "Les bureaux",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "le bureau idéal",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "salle de réunion",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "Le coin repos, Le coin repas, Les lieux de stockage",
-              style: TextStyle(fontSize: 14),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "La cave, les archives",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.teal[100]),
-            alignment: Alignment.center,
-            child: const Text(
-              "Récapitulation des bases",
-              style: TextStyle(fontSize: 18),
-            ),
-          ),
-        ],
+          );
+        }),
       ),
     );
   }
+  // Container(
+  //   //https://stackoverflow.com/questions/45724567/flutter-boxdecoration-s-background-color-overrides-the-containers-background-co
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "Start",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "1",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "2",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "3",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "4",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "5",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "6",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "Réponse quiz 1-6",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "Accueil",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "réponses et bonne pratique",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "Les RH et admin",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "réponses et bonne pratique",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "Les bureaux",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "le bureau idéal",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "salle de réunion",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "Le coin repos, Le coin repas, Les lieux de stockage",
+  //     style: TextStyle(fontSize: 14),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "La cave, les archives",
+  //     style: TextStyle(fontSize: 20),
+  //   ),
+  // ),
+  // Container(
+  //   decoration: BoxDecoration(
+  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
+  //   alignment: Alignment.center,
+  //   child: const Text(
+  //     "Récapitulation des bases",
+  //     style: TextStyle(fontSize: 18),
+  //   ),
+  // ),
+  //     ),
+  //   );
+  // }
 }
