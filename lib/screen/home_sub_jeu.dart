46cff49f2a82471c3e748c248dad3cd4cdab5f3d
LeonardFavre95 <leonard.favre@students.hevs.ch>
2022-07-22 20:24:21 +0200

    maj container

--- lib/screen/home_sub_jeu.dart
+++ lib/screen/home_sub_jeu.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 
 class HomeSubJeu extends StatelessWidget {
-  const HomeSubJeu({Key? key, required this.items}) : super(key: key);
-  final List<String> items;
+  const HomeSubJeu({Key? key}) : super(key: key);
+  static const List<String> buttonText = <String>[
+    'Start',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    'Réponse quiz 1-6',
+    'Accueil',
+    'réponses et bonne pratique',
+    'Les RH et admin',
+    'réponses et bonne pratique',
+    'Les bureaux',
+    'le bureau idéal',
+    'salle de réunion',
+    'Le coin repos, Le coin repas, Les lieux de stockage',
+    'La cave, les archives',
+    'Récapitulation des bases'
+  ];
   @override
   Widget build(BuildContext context) {
     //child: Text(context.widget, style: Theme.of(context).textTheme.headline6),
@@ -25,176 +44,16 @@ class HomeSubJeu extends StatelessWidget {
                 color: Colors.teal[100],
               ),
               alignment: Alignment.center,
+              child: Text(
+                //https://medium.com/@AnInsightfulTechie/flutter-displaying-dynamic-contents-using-listview-builder-f2cedb1a19fb
+                buttonText[index],
+                style: const TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
             ),
           );
         }),
       ),
     );
   }
-  // Container(
-  //   //https://stackoverflow.com/questions/45724567/flutter-boxdecoration-s-background-color-overrides-the-containers-background-co
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "Start",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "1",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "2",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "3",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "4",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "5",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "6",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "Réponse quiz 1-6",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "Accueil",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "réponses et bonne pratique",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "Les RH et admin",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "réponses et bonne pratique",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "Les bureaux",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "le bureau idéal",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "salle de réunion",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "Le coin repos, Le coin repas, Les lieux de stockage",
-  //     style: TextStyle(fontSize: 14),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "La cave, les archives",
-  //     style: TextStyle(fontSize: 20),
-  //   ),
-  // ),
-  // Container(
-  //   decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.black), color: Colors.teal[100]),
-  //   alignment: Alignment.center,
-  //   child: const Text(
-  //     "Récapitulation des bases",
-  //     style: TextStyle(fontSize: 18),
-  //   ),
-  // ),
-  //     ),
-  //   );
-  // }
 }
