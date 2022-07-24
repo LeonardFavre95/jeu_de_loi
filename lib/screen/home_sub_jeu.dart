f33e485362df2c5fc94dd2d07ca09d23e2d8fb58
LeonardFavre95 <leonard.favre@students.hevs.ch>
2022-07-24 13:11:14 +0200

    Quizz widget

--- lib/screen/home_sub_jeu.dart
+++ lib/screen/home_sub_jeu.dart
@@ -1,7 +1,17 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: avoid_unnecessary_containers
 
-class HomeSubJeu extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:jeu_de_loi/screen/home_sub_jeu_quizz.dart';
+
+class HomeSubJeu extends StatefulWidget {
   const HomeSubJeu({Key? key}) : super(key: key);
+
+  @override
+  _HomeSubJeuState createState() => _HomeSubJeuState();
+}
+
+class _HomeSubJeuState extends State<HomeSubJeu> {
+  late int _selectedIndex;
   static const List<String> buttonText = <String>[
     'Start',
     '1',
@@ -22,38 +32,59 @@ class HomeSubJeu extends StatelessWidget {
     'La cave, les archives',
     'Récapitulation des bases'
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedIndex = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
-    //child: Text(context.widget, style: Theme.of(context).textTheme.headline6),
-    return InkWell(
-      //onTap: () => ,
-      child: GridView.count(
-        reverse: true,
-        crossAxisCount: 3,
-        padding: const EdgeInsets.all(20),
-        children: List.generate(18, (index) {
-          return Padding(
-            padding: const EdgeInsets.all(0),
-            child: Container(
-              //https://stackoverflow.com/questions/45724567/flutter-boxdecoration-s-background-color-overrides-the-containers-background-co
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 0.7,
-                ),
-                color: Colors.teal[100],
+    return Scaffold(
+      //child: Text(context.widget, style: Theme.of(context).textTheme.headline6),
+      body: _game(context),
+    );
+  }
+
+  Widget _game(BuildContext context) {
+    return GridView.count(
+      reverse: true,
+      crossAxisCount: 3,
+      padding: const EdgeInsets.all(20),
+      children: List.generate(18, (index) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const HomeSubJeuQuizz()));
+
+            // setState(() {
+            //   _selectedIndex = index;
+            // });
+            // print(buttonText[index]);
+            // print(index);
+          },
+          child: Container(
+            //https://stackoverflow.com/questions/45724567/flutter-boxdecoration-s-background-color-overrides-the-containers-background-co
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 0.7,
               ),
-              alignment: Alignment.center,
-              child: Text(
-                //https://medium.com/@AnInsightfulTechie/flutter-displaying-dynamic-contents-using-listview-builder-f2cedb1a19fb
-                buttonText[index],
-                style: const TextStyle(fontSize: 20),
-                textAlign: TextAlign.center,
-              ),
+              color: Colors.teal[100],
             ),
-          );
-        }),
-      ),
+            alignment: Alignment.center,
+            child: Text(
+              //https://medium.com/@AnInsightfulTechie/flutter-displaying-dynamic-contents-using-listview-builder-f2cedb1a19fb
+              buttonText[index],
+              style: const TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        );
+      }),
     );
   }
 }
