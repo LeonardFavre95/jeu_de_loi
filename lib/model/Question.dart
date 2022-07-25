fa0160865d26be0c30607650a9e3f3227db68517
LeonardFavre95 <leonard.favre@students.hevs.ch>
2022-07-25 23:14:34 +0200

    Question Show

---
+++ lib/model/Question.dart
@@ -0,0 +1,81 @@
+//https://github.com/Yukta-Koli/Quiz-App/blob/main/lib/models/Questions.dart
+
+class Question {
+  final int id, answer;
+  final String question;
+  final List<String> options;
+
+  Question({
+    required this.id,
+    required this.question,
+    required this.answer,
+    required this.options,
+  });
+}
+
+const List sampleData = [
+  {
+    "id": 1,
+    "question": "Pour jouer au jeu de loi je dois vérifier au préalable :",
+    "options": [
+      "Que mon pays est bien signataire de la convention",
+      "Que moi-même ou mon organisation soyons prêts à réfléchir à nos procédures et à nos processus internes",
+      "Que mon chef ou ma cheffe soit d’accord"
+    ],
+    "answer_index": 1,
+  },
+  {
+    "id": 2,
+    "question":
+        "Les données dites personnelles sont toutes les informations qui permet d’identifier une personne, directement ou indirectement, donc :",
+    "options": [
+      "Cela concerne toutes sortes de populations : les salariés d’une entreprise, ses clients, les demandeurs d’emploi, les patients d’un hôpital, les citoyennes et citoyens, etc.",
+      "Cela ne concerne que les données d’identité, de tout un chacun",
+      "Cela ne concerne que des informations qui sont en relation directe avec le nom et le prénom des personnes"
+    ],
+    "answer_index": 0,
+  },
+  {
+    "id": 3,
+    "question":
+        "Lorsque l’on a défini les données personnelles que l’on traite dans une organisation : ",
+    "options": [
+      "On doit les sécuriser, mais seulement les données numériques",
+      "On doit appliquer une série de principes quel que soit le traitement que l’on fait avec ces données",
+      "On peut tout faire, pour autant qu’elles ne sortent pas de l’organisation"
+    ],
+    "answer_index": 1,
+  },
+  {
+    "id": 4,
+    "question":
+        "Quand on parle de traiter les données personnelles, on entend par là : ",
+    "options": [
+      "les traiter concrètement, c’est-à-dire les manipuler, les déplacer, les dupliquer",
+      "Tout ce que l’on fait avec ces données personnelles dès leur collecte et jusqu’à leur destruction",
+      "Mettre des données dans un fichier informatique"
+    ],
+    "answer_index": 1,
+  },
+  {
+    "id": 5,
+    "question":
+        "D’ailleurs quand on parle de traitement de données personnelles :",
+    "options": [
+      "Cela exclut la simple consultation d’un fichier, par exemple",
+      "Cela exclut le fait de les confier à un hébergeur externe",
+      "Cela comprend de manière générale tout ce que l’on fait avec les données personnelles et quel que soit leur format (papier ou numérique)"
+    ],
+    "answer_index": 2,
+  },
+  {
+    "id": 6,
+    "question": "Pour appliquer les bonnes pratiques de la convention 108 :",
+    "options": [
+      "Il faut se demander quel est le but d’un traitement, pour adapter ensuite le type de données que l’on va traiter, le type de traitement que l’on va prévoir, et la durée pendant laquelle on va conserver ses données",
+      "Il suffit que le pays soit signataire de la convention, et que l’on s’engage à la respecter",
+      "Il faut avoir fait des études de juriste"
+    ],
+    "answer_index": 0,
+  }
+];
--- lib/screen/home_sub_jeu_quizz.dart
+++ lib/screen/home_sub_jeu_quizz.dart
@@ -2,39 +2,87 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:jeu_de_loi/controller/questionCcontroller.dart';
+import 'package:jeu_de_loi/model/Question.dart';
+import 'option.dart';
 
 class HomeSubJeuQuizz extends StatelessWidget {
   final int selectedIndex;
   final String selectedText;
+  final Question question;
+
   const HomeSubJeuQuizz(
-      {Key? key, required this.selectedIndex, required this.selectedText})
+      {Key? key,
+      required this.selectedIndex,
+      required this.selectedText,
+      required this.question})
       : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    //https://stackoverflow.com/questions/51360481/how-can-i-to-load-and-query-local-json-data-in-flutter-mobile-app
-    
+    //https://medium.com/swlh/the-simplest-way-to-pass-and-fetch-data-between-stateful-and-stateless-widgets-pages-full-2021-c5dbce8db1db
+    //https://github.com/Yukta-Koli/Quiz-App/blob/main/lib/screens/quiz/components/question_card.dart
     return Scaffold(
-        appBar: AppBar(title: Text(selectedText)), body: _quizz(selectedText));
+        appBar: AppBar(title: Text(selectedText)),
+        body: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 5),
+          padding: const EdgeInsets.all(5.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: Column(
+            children: [
+              Text(
+                question.question,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline1
+                    ?.copyWith(color: Colors.white),
+              ),
+              const SizedBox(height: 10),
+              //  ...List.generate(question.options.length, (index) => Option())
+            ],
+          ),
+        ));
   }
 }
 
-Widget _quizz(String se) {
-  return Expanded(
-    child: IndexedStack(
-      index: 0,
-      children: <Widget>[
-        Container(
-            child: Center(
-                child: Text(
-          ,
-          style: const TextStyle(fontSize: 12),
-          textAlign: TextAlign.center,
-        )))
-      ],
-    ),
-  );
-}
+// Widget _quizz() {
+//   return Scaffold(
+//     body: Center(child: Text('$selectedIndex')),
+//   );
+// return Expanded(
+//   child: IndexedStack(
+//     index: 0,
+//     children: <Widget>[
+//       Center(
+//           child: Text(
+//         '$selectedIndex',
+//         //_items[selectedIndex]["1"],
+//         style: const TextStyle(fontSize: 12),
+//         textAlign: TextAlign.center,
+//       ))
+//     ],
+//   ),
+// );
+// }
 
+// class _HomeSubJeuQuizzState extends State<HomeSubJeuQuizz> {
 
+//https://stackoverflow.com/questions/51360481/how-can-i-to-load-and-query-local-json-data-in-flutter-mobile-app
+// Future<void> readJson() async {
+//   final String response = await rootBundle.loadString('asset/question.json');
+//   final data = await json.decode(response);
+//   setState(() {
+//     _items = data[widget.selectedText];
+//   });
+// }
 
+//   @override
+//   void initState() {
+//     super.initState();
+//     // readJson();
+//   }
 
+// }
