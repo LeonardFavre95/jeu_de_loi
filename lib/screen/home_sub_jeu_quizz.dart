466a75e03d998ba36e3bf5b59b9ea3058d152265
LeonardFavre95 <leonard.favre@students.hevs.ch>
2022-07-28 00:50:31 +0200

    Quizz answer

--- lib/screen/home_sub_jeu_quizz.dart
+++ lib/screen/home_sub_jeu_quizz.dart
@@ -4,9 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:jeu_de_loi/controller/questionCcontroller.dart';
 import 'package:jeu_de_loi/model/Question.dart';
-import 'option.dart';
 
-class HomeSubJeuQuizz extends StatelessWidget {
+class HomeSubJeuQuizz extends StatefulWidget {
   final int selectedIndex;
   final String selectedText;
   final Question question;
@@ -19,34 +18,99 @@ class HomeSubJeuQuizz extends StatelessWidget {
       : super(key: key);
 
   @override
+  HomeSubJeuQuizzState createState() => HomeSubJeuQuizzState();
+}
+
+class HomeSubJeuQuizzState extends State<HomeSubJeuQuizz> {
+  bool isButtonpressed1 = false;
+  bool isButtonpressed2 = false;
+  bool isButtonpressed3 = false;
+
+  Color buttonColor = Colors.grey;
+
+  Color getTheRightColor(int option, bool isbuttonpressed) {
+    switch (isbuttonpressed) {
+      case true:
+        if (widget.question.answer == option) {
+          return Colors.green;
+        } else {
+          return Colors.red;
+        }
+      case false:
+        return Colors.grey;
+      default:
+        return Colors.grey;
+    }
+
+    // if (!isButtonpressed1 || !isButtonpressed2 || !isButtonpressed3) {
+    //   return Colors.grey;
+    // } else if (widget.question.answer == option) {
+    //   return Colors.green;
+    // } else {
+    //   return Colors.red;
+    // }
+  }
+
+  @override
   Widget build(BuildContext context) {
     //https://medium.com/swlh/the-simplest-way-to-pass-and-fetch-data-between-stateful-and-stateless-widgets-pages-full-2021-c5dbce8db1db
     //https://github.com/Yukta-Koli/Quiz-App/blob/main/lib/screens/quiz/components/question_card.dart
     return Scaffold(
-        appBar: AppBar(title: Text(selectedText)),
+        appBar: AppBar(
+          title: Text(widget.selectedText),
+          centerTitle: true,
+        ),
         body: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 5),
-          padding: const EdgeInsets.all(5.0),
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          padding: const EdgeInsets.all(10),
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(25),
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                question.question,
-                style: Theme.of(context)
-                    .textTheme
-                    .headline1
-                    ?.copyWith(color: Colors.white),
+                style: const TextStyle(fontSize: 20),
+                widget.question.question,
               ),
-              const SizedBox(height: 10),
-              //  ...List.generate(question.options.length, (index) => Option())
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: getTheRightColor(0, isButtonpressed1)),
+                onPressed: () {
+                  setState(() {
+                    isButtonpressed1 = true;
+                  });
+                },
+                child: Text(widget.question.options[0]),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: getTheRightColor(1, isButtonpressed2)),
+                onPressed: () {
+                  setState(() {
+                    isButtonpressed2 = true;
+                  });
+                },
+                child: Text(widget.question.options[1]),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: getTheRightColor(2, isButtonpressed3)),
+                onPressed: () {
+                  setState(() {
+                    isButtonpressed3 = true;
+                  });
+                },
+                child: Text(widget.question.options[2]),
+              ),
             ],
           ),
         ));
   }
 }
+
 
 // Widget _quizz() {
 //   return Scaffold(
